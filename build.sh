c256d09a5ecb21231ba7cd276b0e39af4803bcd3
xyen8180 <xyen8180@gmail.com>
2021-12-17 15:36:02 +0530

    android

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Just a basic script U can improvise lateron asper ur need xD 
+
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp -b twrp-11"
+
+
+DEVICE=star
+DT_LINK="https://github.com/xyen8180/test-tree -b aosp"
+DT_PATH=device/xiaomi/$DEVICE
+SD_LINK="https://github.com/xyen8180/android_device_xiaomi_sm8350-common -b aosp"
+SD_PATH="device/xiaomi/sm8350-common"
+echo " ===+++ Setting up Build Environment +++==="
+apt install openssh-server -y
+apt update --fix-missing
+apt install openssh-server -y
+mkdir android-kernel && cd android-kernel
+
+echo " ===+++ Syncing Recovery Sources +++==="
+echo " ===+++ Cloning Manifest  +++==="
+repo init -u https://android.googlesource.com/kernel/manifest -b common-android11-5.4
+repo sync
+ls
